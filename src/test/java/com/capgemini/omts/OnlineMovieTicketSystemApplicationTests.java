e74ed0fd122d9e3dd6356404c2615afb6ef9bdc4
ykvsai <ykvsai@users.noreply.github.com>
2020-09-24 13:50:06 +0530

    updated

--- src/test/java/com/capgemini/omts/OnlineMovieTicketSystemApplicationTests.java
+++ src/test/java/com/capgemini/omts/OnlineMovieTicketSystemApplicationTests.java
@@ -1,7 +1,13 @@
 package com.capgemini.omts;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -23,49 +29,51 @@ import com.capgemini.omts.service.TheatreServiceImpl;
 class OnlineMovieTicketSystemApplicationTests {
 
 	@Mock
-	private Theatre theatre;
-	
-	@Mock
 	private TheatreRepository repo;
-	
+
 	@InjectMocks
 	private TheatreServiceImpl ser;
-	
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test
 	public void addTheaterTest1() throws TheatreAlreadyExists {
-		//Theatre theatre = new Theatre();
-		theatre.setTheatreId(1002);
+		Theatre theatre = new Theatre();
+		theatre.setTheatreId(1001);
 		theatre.setTheatreName("priya");
 		theatre.setTheatreCity("Tenali");
 		theatre.setManagerName("swami");
 		theatre.setManagerContact("swami@xyz.com");
-		//Theatre th = ser.addTheatre(theatre);
-		//System.out.println("th"+th);
 		when(ser.addTheatre(theatre)).thenReturn(theatre);
-		assertEquals(null,theatre);
-		
-		//Mockito.verify(ser.addTheatre(theatre));
-		//System.out.println("th"+th);
-		
-		
+		assertNotEquals(null, ser.addTheatre(theatre));
 	}
-	
+
 	@Test
-	public void viewTheatersTest()throws TheatreNotFoundException, TheatreAlreadyExists{
-		
-		theatre.setTheatreId(1010);
-		theatre.setTheatreName("priya");
-		theatre.setTheatreCity("Tenali");
-		theatre.setManagerName("swami");
-		theatre.setManagerContact("swami@xyz.com");
-		System.out.println("viewlist"+ser.viewTheatres().size());
-		ser.addTheatre(theatre);
-		assertEquals(5, ser.viewTheatres().size());
+	public void viewTheatersTest() throws TheatreNotFoundException, TheatreAlreadyExists {
+		Theatre theatre1 = new Theatre();
+		theatre1.setTheatreId(1003);
+		theatre1.setTheatreName("MBD");
+		theatre1.setTheatreCity("Jalandhar");
+		theatre1.setManagerName("keshav");
+		theatre1.setManagerContact("keshav@gca.com");
+
+		Theatre theatre2 = new Theatre();
+		theatre2.setTheatreId(1002);
+		theatre2.setTheatreName("prime");
+		theatre2.setTheatreCity("phagwara");
+		theatre2.setManagerName("deepu");
+		theatre2.setManagerContact("deepu@lpu.com");
+
+		List<Theatre> theatreList = new ArrayList<>();
+		theatreList.add(theatre1);
+		theatreList.add(theatre2);
+
+		when(repo.findAll()).thenReturn(theatreList);
+		assertThat(ser.viewTheatres()).isEqualTo(theatreList);
+		assertEquals(theatreList.size(), 2);
 	}
 
 }
